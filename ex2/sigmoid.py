084a79c875a9425096496f29b13fedbdfa61ebb1
Chris Schlosberg <christopher.schlosberg@monsanto.com>
2018-03-07 19:05:39 -0600

    First commit and upload

---
+++ ex2/sigmoid.py
@@ -0,0 +1,12 @@
+from numpy import e
+
+def sigmoid(z):
+    """computes the sigmoid of z."""
+
+# ====================== YOUR CODE HERE ======================
+# Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+#               vector or scalar).
+
+# =============================================================
+    g = 0
+    return g
